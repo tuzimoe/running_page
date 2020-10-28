093d10fa9b78067720b8f19b43bb42e99a0424c1
TuziMoe <a@tuzi.moe>
2020-10-28 23:49:57 +1300

    mapbox token

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,5 @@
 // const
-const MAPBOX_TOKEN = 'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoid2VlZSIsImEiOiJjamtoanU0Y2cwdXYzM3FtbHV6dW1keTRiIn0.kZyuTrsCYWYp4PDRuRVaaA';
 const MUNICIPALITY_CITIES_ARR = ['北京市', '上海市', '天津市', '重庆市', '香港特别行政区', '澳门特别行政区'];
 const IS_CHINESE = true
 
