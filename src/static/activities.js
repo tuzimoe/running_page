26ff2df25ef09ac9274e7cbd997803f00676aeab
TuziMoe <a@tuzi.moe>
2020-10-30 02:50:53 +1300

    new

--- src/static/activities.js
+++ src/static/activities.js
@@ -11,7 +11,7 @@ const activities = [
     "summary_polyline": "ftf_Fiyoi`@mA_AUqCiA}BiCzH_AzF{@xBNzAk@jAeAZwAdFyMzJqFf@yBo@cEO{D`HoG`FiKoD",
     "average_heartrate": null,
     "average_speed": 1.13051457458447,
-    "streak": 3
+    "streak": 1
   },
   {
     "run_id": 1603879216402,
@@ -25,7 +25,21 @@ const activities = [
     "summary_polyline": "lgd_Fonni`@e@U}@pBqPuDkEqKiDaBeBoF}PcV}BfA{@tCq@YuA{CMeA\\k@]MImAPue@c@wBVgCeMMAWzDFFpBuAb@aBWDiA",
     "average_heartrate": null,
     "average_speed": 1.4299006611905147,
-    "streak": 10
+    "streak": 1
+  },
+  {
+    "run_id": 1603968503762,
+    "name": "run from gpx",
+    "distance": 2958.0848242036514,
+    "moving_time": "0:43:39.002000",
+    "type": "Run",
+    "start_date": "2020-10-29 10:48:23",
+    "start_date_local": "2020-10-29 23:48:23",
+    "location_country": "8, Helvetia Drive, Pinehill, Hibiscus and Bays, Auckland, 0630, New Zealand",
+    "summary_polyline": "`hd_Fmoni`@KbARwAUp@PVc@k@@h@Jo@Ol@@{@Jt@c@k@WzAqALwNwCmFkL}CaB{BmGkO}Su@WiA`@oAtDmA{@qAoHTsj@c@uAZ_Ci@SKbAeAVwJMyAkAj@iExAB",
+    "average_heartrate": null,
+    "average_speed": 1.1294702425594372,
+    "streak": 2
   }
 ];
 
